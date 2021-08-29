9cdca2cf662b2592ce31dad9d603d4e14903ae5d
robert mix <eurotuner1981@gmail.com>
2021-08-29 19:20:30 +0200

    Skeleton menu complete

---
+++ Grade_Manager/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Grade_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+            static void Menu()
+            { 
+                Console.WriteLine("****** Grade Manager *******");
+                Console.WriteLine("Keep track of your grades.");
+                Console.WriteLine("1. View currently stored grades.");
+                Console.WriteLine("2. View class grade average.");
+                Console.WriteLine("3. Add a grade.");
+                Console.WriteLine("4. Edit a grade.");
+                Console.WriteLine("5. Remove a student.");
+                Console.WriteLine("6. View top student.");
+                Console.WriteLine("7. View bottom student.");
+                Console.WriteLine("8. Clear all stored grades.");
+                Console.WriteLine("9. Exit");
+
+                 string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            break;
+                        }
+
+                    case "4":
+                        {
+                            break;
+                        }
+
+                    case "5":
+                        {
+                            break;
+                        }
+
+                    case "6":
+                        {
+                            break;
+                        }
+
+                    case "7":
+                        {
+                            break;
+                        }
+
+                    case "8":
+                        {
+                            break;
+                        }
+
+                    case "9":
+                        {
+                            break;
+                        }
+
+
+                }
+
+
+                    
+            }
+        }
+    }
+}
